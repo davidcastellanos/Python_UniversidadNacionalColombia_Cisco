cf56d03420771dd344fe48a2ac9285b58212a944
davidcastellanos <castellanos.johann@gmail.com>
2020-06-21 11:12:49 -0500

    [Feature] Added new file

---
+++ prueba-strings.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 21 11:06:16 2020
+
+@author: David-Castellanos
+"""
+
+'''
+Prueba con Salida esperada:
+"Estoy"
+ ""aprendiendo""
+ """Python"""
+'''
+print('"Estoy"\n', '""aprendiendo""\n', '"""Python"""')
